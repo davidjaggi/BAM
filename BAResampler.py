3dd46cbdb77a27ebc4234f046294ec226f7cdf84
djaggi <davidjaggi@users.noreply.github.com>
2018-02-19 12:18:42 +0000

    Completed BA Sampler

--- BAResampler.py
+++ BAResampler.py
@@ -1,8 +1,26 @@
 import pandas as pd
-data_frame = pd.read_csv('AUDJPY-2016-01.csv', names=['Symbol', 'Date_Time', 'Bid', 'Ask'],index_col=1, parse_dates=True)
-data_frame.head()
-data_ask =  data_frame['Ask'].resample('15Min').ohlc()
-data_bid =  data_frame['Bid'].resample('15Min').ohlc()
-data_ask.head()
-data_bid.head()
-data_ask_bid=pd.concat([data_ask, data_bid], axis=1, keys=['Ask', 'Bid'])
+
+input_file = "C:/Users/Administrator/Desktop/Data/AUDCAD.FXCM.asc"
+ba_ready_file = "C:/Users/Administrator/Desktop/BaData/BA_AUDCAD_15min.txt"
+
+df = pd.read_csv(input_file,index_col=0, parse_dates=[[0,1]])
+df.head()
+df_open =  df['Open'].resample('15Min').ohlc()
+df_high =  df['High'].resample('15Min').ohlc()
+df_low =  df['Low'].resample('15Min').ohlc()
+df_close =  df['Close'].resample('15Min').ohlc()
+df_vol =  df['TotalVolume'].resample('15Min').ohlc()
+
+df_resample = pd.concat([df_open,df_high,df_low,df_close])
+
+
+
+
+# ----------------------
+#  Do NOT Change
+#
+fh = open(ba_ready_file, 'w')
+fh.write("Date,Time,Open,High,Low,Close,Vol,OI\n")
+for d,o,h,l,c in zip(df_resample.index,df_resample['open'],df_resample['high'],df_resample['low'],df_resample['close']):
+ fh.write("%s,%s,%.5f,%.5f,%.5f,%.5f,%d,%d\n" % (d.strftime('%m/%d/%Y'),d.strftime('%H:%M'),o,h,l,c,0,0))
+fh.close()
