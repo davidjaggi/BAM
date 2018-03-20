e828f7b8283bdef4c1b7e81fb01ed2cf8d1bcc7a
davidjaggi <davidjaggi@users.noreply.github.com>
2018-03-20 16:23:02 +0000

    Updated Maschine Learning

--- MaschineLearning.py
+++ MaschineLearning.py
@@ -1,10 +1,6 @@
 # https://gist.github.com/TeamAuquan/b90b348caf4573ebf44a5751ef633b7d#file-apply-ml-to-trading-ipynb
 # Import packages
 
- 
-import numpy
-import scipy
-import sklearn
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
@@ -12,4 +8,8 @@ import matplotlib.pyplot as plt
 import os
 
 # Get FX prices
-fx = "C:/Users/Administrator/Desktop/BaData/BA_GBPUSD_15min.txt"
+fx_fileName = "C:/Users/David Jaggi/Google Drive/Eiffeltower Capital/BaData/BA_GBPUSD_15min.txt"
+data = pd.read_csv(fx_fileName, index_col = [[0,1]], parse_dates=[[0,1]])
+data = pd.read
+head(data)
+data.plot()
