9052d87d0635e397c805c7f49f7b2a05ab3015af
davidjaggi <davidjaggi@users.noreply.github.com>
2018-08-05 09:30:54 +0100

    added to App

--- BAM_App/BAM_App.py
+++ BAM_App/BAM_App.py
@@ -9,42 +9,78 @@ Last edited: August 2018
 """
 import sys
 import os
-
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtPrintSupport import *
-from PyQt5.QtGui import QIcon
+import random
+import matplotlib
+# Make sure that we are using QT5
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtWidgets import QFileDialog
 from PyQt5 import uic
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
 from matplotlib.figure import Figure
-from matplotlib import rcParams
+
+from MplWidget import MatplotlibWidget
+
+import numpy as np
 
 
-class BAM_App(QMainWindow):
+class BAM_App(QtWidgets.QMainWindow):
     def __init__(self, parent = None):
-        QMainWindow.__init__(self,parent)
+        QtWidgets.QMainWindow.__init__(self,parent)
         uic.loadUi('C:/Users/David Jaggi/Documents/GitHub/BAM/BAM_App/userInterface.ui', self)
+    
+        self.plotData.clicked.connect(self.plot_data)
+        self.clearPlot.clicked.connect(self.clear_plot)
 
-        openFile = QAction("actionOpen", self)
-        openFile.triggered.connect(self.file_open)
+        self.actionOpen.triggered.connect(self.openFileNameDialog)
+        self.actionQuit.triggered.connect(self.fileQuit)
+        self.actionInfo.triggered.connect(self.about)
+    
+    def plot_data(self):
+        x=np.random.rand(100)
+        y=np.random.rand(100)
+        self.plotWidget.axes.plot(x,y)
+        self.plotWidget.draw()
+    
+    def clear_plot(self):
+        self.plotWidget.axes.clear()
+        self.plotWidget.draw()
 
-    def file_open(self):
-        name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
-        file = open(name,'r')
+    def openFileNameDialog(self):
+        try: 
+            filename = QtWidgets.QFileDialog.getOpenFileName(
+               self, 'Open File', '', 'Images (*.png *.xpm *.jpg)',
+               None, QtGui.QFileDialog.DontUseNativeDialog)
+            self.lineEdit.setText(filename)
+        except:
+            pass
+    
 
-        self.editor()
+    
+    def fileQuit(self):
+        self.close()
 
-        with file:
-            text = file.read()
-            self.textEdit.setText(text)
+    def closeEvent(self, ce):
+        self.fileQuit()
 
+    def about(self):
+        QtWidgets.QMessageBox.about(self, "About",
+                                    """embedding_in_qt5.py example
+Copyright 2005 Florent Rougon, 2006 Darren Dale, 2015 Jens H Nielsen
 
+This program is a simple example of a Qt5 application embedding matplotlib
+canvases.
 
+It may be used and modified with no restriction; raw copies as well as
+modified versions may be distributed without limitation.
+
+This is modified from the embedding in qt4 example to show the difference
+between qt4 and qt5"""
+                                )
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = BAM_App()
     window.show()
     app.exec_()
